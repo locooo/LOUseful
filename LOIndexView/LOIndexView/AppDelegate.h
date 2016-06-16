e5bc78f631e75a3b27b473f4fdc50c2ff30bc8ac
locoomm@gmail.com <L@locooMac.local>
2016-06-16 20:07:32 +0800

    add

---
+++ LOIndexView/LOIndexView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOIndexView
+//
+//  Created by locoo on 16/6/15.
+//  Copyright © 2016年 locoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOIndexView/LOIndexView/TestModel.m
@@ -0,0 +1,13 @@
+//
+//  TestModel.m
+//  LOIndexView
+//
+//  Created by locoo on 16/6/15.
+//  Copyright © 2016年 locoo. All rights reserved.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
